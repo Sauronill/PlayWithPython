4a6949ada59c46070bdb0a168f5056cbdac82ddc
zangyinxu <zangyinxu@xtour.com>
2022-01-28 19:07:26 +0800

    change request url

--- flask-vue/adminjs/src/api/banners.js
+++ flask-vue/adminjs/src/api/banners.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 
 export const fetchData = query => {
     return request({
-        url: 'http://127.0.0.1:5000/',
+        url: 'http://127.0.0.1:5000/test',
         method: 'get',
         params: query
 
--- flask-vue/adminjs/src/api/banners2.js
+++ flask-vue/adminjs/src/api/banners2.js
@@ -2,7 +2,7 @@ import request from '../utils/request';
 
 export const fetchData2 = query => {
     return request({
-        url: 'http://127.0.0.1:5000/',
+        url: 'http://127.0.0.1:5000/test',
         method: 'get',
         params: {
             ID:12345
